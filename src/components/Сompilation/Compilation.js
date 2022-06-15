f5b2d3a6d999b9f08c464ac8199bc22d2fcd6c6d
Anna Krasnovid <krasnovidanka@yandex.ru>
2022-06-15 12:29:06 +0300

    movie

--- src/components/Сompilation/Compilation.js
+++ src/components/Сompilation/Compilation.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './Compilation.css';
+import MovieCard from '../MovieCard/MovieCard';
 
 function Compilation() {
   return (
     <section className='compilation'>
       <h2 className='compilation__title'>Лучшие фильмы подборки</h2>
       <div className='compilation__movies'>
+<MovieCard />
+<MovieCard />
+<MovieCard />
+<MovieCard />
+<MovieCard />
+<MovieCard />
+<MovieCard />
+<MovieCard />
+<MovieCard />
+<MovieCard />
 
       </div>
-
     </section>
   )
 }
