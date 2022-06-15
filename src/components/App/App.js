16a3b8157d5d4d2795834cc0b383324c75390471
Anna Krasnovid <krasnovidanka@yandex.ru>
2022-06-15 13:41:44 +0300

    bg

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,15 @@ import Main from '../Main/Main';
 
 function App() {
   return (
+
     <div className="page">
       <Header />
       <Routes>
         <Route path="/main" element={<Main />} />
       </Routes>
     </div>
+
+
   )
 }
 
