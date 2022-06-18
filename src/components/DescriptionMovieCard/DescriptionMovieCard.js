09f5a671d989410e5adc0ad4666bbea7fe921262
Anna Krasnovid <krasnovidanka@yandex.ru>
2022-06-18 19:49:33 +0300

    style description movie

--- src/components/DescriptionMovieCard/DescriptionMovieCard.js
+++ src/components/DescriptionMovieCard/DescriptionMovieCard.js
@@ -14,7 +14,7 @@ function DescriptionMovieCard() {
               <span className='about-movie__estimation'>8.9</span>
               <span className='about-movie__owner'>Кинопоиск</span>
             </div>
-            <div className='about-movie__box-rating'>
+            <div className='about-movie__box-rating about-movie__box-rating_type_imdb'>
               <span className='about-movie__estimation'>8</span>
               <span className='about-movie__owner'>IMDb</span>
             </div>
@@ -23,7 +23,7 @@ function DescriptionMovieCard() {
 
         <div className='about-movie__container'>
           <h3 className='about-movie__title'>Гениальное ограбление (2021)</h3>
-          <p className='about-movie__title-en'></p>
+          <p className='about-movie__title-en'>Гениальное ограбление (2021)</p>
 
           <ul className='about-movie__box-data'>
             <li className='about-movie__box-info'>
@@ -46,26 +46,26 @@ function DescriptionMovieCard() {
 
           <ul className='about-movie__box-main'>
             <li className='about-movie__box-info-main'>
-              <p className='about-movie__heading'>Жанр</p>
-              <p className='about-movie__info'>Боевик, Комедия, Драма</p>
+              <p className='about-movie__heading-main'>Жанр</p>
+              <p className='about-movie__info-main'>Боевик, Комедия, Драма</p>
             </li>
             <li className='about-movie__box-info-main'>
-              <p className='about-movie__heading'>Жанр</p>
-              <p className='about-movie__info'>Боевик, Комедия, Драма</p>
+              <p className='about-movie__heading-main'>Жанр</p>
+              <p className='about-movie__info-main'>Боевик, Комедия, Драма</p>
             </li>
             <li className='about-movie__box-info-main'>
-              <p className='about-movie__heading'>Жанр</p>
-              <p className='about-movie__info'>Боевик, Комедия, Драма</p>
+              <p className='about-movie__heading-main'>Жанр</p>
+              <p className='about-movie__info-main'>Боевик, Комедия, Драма</p>
             </li>
             <li className='about-movie__box-info-main'>
-              <p className='about-movie__heading'>Жанр</p>
-              <p className='about-movie__info'>Боевик, Комедия, Драма</p>
+              <p className='about-movie__heading-main'>Жанр</p>
+              <p className='about-movie__info-main'>Боевик, Комедия, Драма</p>
             </li>
           </ul>
 
           <div className='about-movie__box-description'>
             <p className='about-movie__title-description'>О чем фильм “Гениальное ограбление (2021)”</p>
-            <p className='about-movie__description'>Идейные соображения высшего порядка, а также сложившаяся структура организации
+            <p className='about-movie__info'>Идейные соображения высшего порядка, а также сложившаяся структура организации
               требуют от нас анализа направлений прогрессивного развития.
               Равным образом постоянное информационно-пропагандистское обеспечение нашей деятельности
               влечет за собой процесс внедрения и модернизации направлений прогрессивного развития.
