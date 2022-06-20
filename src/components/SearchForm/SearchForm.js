bd5daefb43e9702a95cca4fba412182200ed6e7d
Anna Krasnovid <krasnovidanka@yandex.ru>
2022-06-20 12:06:51 +0300

    search

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,19 +4,45 @@ import './SearchForm.css';
 import { useLocation } from 'react-router-dom';*/
 
 function SearchForm() {
+  const [isActiveSearchClass, setIsActiveSearchClass] = React.useState(false);
+  const [isActiveInput, setIsActiveInput] = React.useState(false);
+
+  function handleMouseEnter() {
+    setIsActiveSearchClass(true)
+  }
+
+  function handleMouseLeave() {
+    setIsActiveSearchClass(false)
+  }
+
+  function handleOnFocusInput() {
+    setIsActiveInput(true)
+  }
+
+  function handleOnBlurInput() {
+    setIsActiveInput(false)
+  }
 
   return (
-    <section className='search'>
-      <form className="search__form" noValidate>
+    <section className='search' >
+      <form
+      className={`search__form ${isActiveSearchClass || isActiveInput ? 'search__form_active' : ''}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onFocus={handleOnFocusInput}
+      onBlur={handleOnBlurInput}
+      noValidate
+      >
         <input
-          id="search"
-          name="search"
-          className="search__input"
-          type="text"
-          placeholder="Фильм"
+          id='search'
+          name='search'
+          className='search__input'
+          type='text'
+          placeholder='Фильм'
+          minLength='1'
         /*value={values.search || ''}*/
         />
-        <button className="search__button"></button>
+        <button className={`search__button ${isActiveInput ? 'search__button_active' : ''}`}></button>
       </form>
       <span id='search-input-error' className='search__error'></span>
     </section>
@@ -24,3 +50,4 @@ function SearchForm() {
 }
 
 export default SearchForm;
+//
