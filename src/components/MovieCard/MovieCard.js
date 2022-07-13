152ce808d8495b5ea88a3641f06ce93ca54f4a3a
Anna Krasnovid <krasnovidanka@yandex.ru>
2022-07-13 12:08:13 +0300

    edit movie, add rating

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -7,6 +7,16 @@ function MovieCard() {
     <li className='movie'>
       <div className='movie__box'>
         <img className='movie__img' src={ImgMovie} alt='Фильм'></img>
+        <div className='movie__container'>
+          <div className='movie__box-rating'>
+            <span className='movie__rating-icon movie__rating-icon_bg_orange'></span>
+            <span className='movie__rating-mark movie__rating-mark_bg_orange'>10</span>
+          </div>
+          <div className='movie__box-rating'>
+            <span className='movie__rating-icon movie__rating-icon_bg_yellow'></span>
+            <span className='movie__rating-mark movie__rating-mark_bg_yellow'>9.0</span>
+          </div>
+        </div>
       </div>
       <h3 className='movie__title'>Лучшие фильмы подборки</h3>
       <p className='movie__description'>Лучшие фильмы подборки</p>
